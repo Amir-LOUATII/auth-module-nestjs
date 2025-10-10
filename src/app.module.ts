ee4ba356f373d963af916a390871df6b129f5cbe
Amir_Louati25 <ameer.louati@gmail.com>
2025-10-10 21:40:41 +0100

    Add user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoreModule } from './core/core.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     }),
     AuthModule,
     UsersModule,
+    CoreModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,10 @@
+import { BaseEntity } from 'src/core/entities/base.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column({ unique: true })
+  email: string;
+  @Column()
+  password: string;
+}
