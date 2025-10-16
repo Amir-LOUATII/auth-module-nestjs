d9ba2e8ae63c871682a3b9959b47642f409496b5
Amir_Louati25 <ameer.louati@gmail.com>
2025-10-16 20:55:10 +0100

    feat: add users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  async findUserByEmail(email: string): Promise<User | null> {
+    return this.userRepo.findOne({ where: { email } });
+  }
+
+  async findUserById(email: string): Promise<User | null> {
+    return this.userRepo.findOne({ where: { email } });
+  }
+
+  async createUser(user: Partial<User>): Promise<User> {
+    return this.userRepo.save(user);
+  }
+
+  async checkDuplicateEmail(email: string): Promise<boolean> {
+    const user = await this.userRepo.findOne({ where: { email } });
+    return !!user;
+  }
+}
